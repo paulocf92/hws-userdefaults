d46e34d6cf49ecf9f86158d1efad54675828c08e
Paulo Filho <pcf.feat@gmail.com>
2022-09-26 10:10:24 -0400

    study: change Person to conform to archiving protocols before storing them to user defaults

--- NamesToFaces/Person.swift
+++ NamesToFaces/Person.swift
@@ -7,12 +7,26 @@
 
 import UIKit
 
-class Person: NSObject {
+class Person: NSObject, NSSecureCoding {
     var name: String
     var image: String
     
     init(name: String, image: String) {
         self.name = name
         self.image = image
+    }
+    
+    required init(coder decoder: NSCoder) {
+        name = decoder.decodeObject(of: [NSString.self], forKey: "name") as? String ?? ""
+        image = decoder.decodeObject(of: [NSString.self], forKey: "image") as? String ?? ""
+    }
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: "name")
+        coder.encode(image, forKey: "image")
+    }
+    
+    static var supportsSecureCoding: Bool {
+        return true
     }
 }
--- NamesToFaces/ViewController.swift
+++ NamesToFaces/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         super.viewDidLoad()
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+        
+        let defaults = UserDefaults.standard
+        
+        if let savedPeople = defaults.object(forKey: "people") as? Data {
+            do {
+                let decodedPeople = try NSKeyedUnarchiver.unarchivedObject(ofClasses: [NSArray.self, Person.self], from: savedPeople) as! [Person]
+                    people = decodedPeople
+            } catch { print(error) }
+        }
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -62,6 +71,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         
         let person = Person(name: "Unknown", image: imageName)
         people.append(person)
+        save()
         collectionView.reloadData()
         
         dismiss(animated: true)
@@ -82,6 +92,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         })
         ac.addAction(UIAlertAction(title: "Delete", style: .destructive) { [weak self] _ in
             self?.people.remove(at: indexPath.item)
+            self?.save()
             self?.collectionView.reloadData()
         })
         
@@ -97,11 +108,19 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
             guard let newName = ac?.textFields?[0].text else { return }
             person.name = newName
+            self?.save()
             self?.collectionView.reloadData()
         })
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         
         present(ac, animated: true)
+    }
+    
+    func save() {
+        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: people, requiringSecureCoding: true) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "people")
+        }
     }
 }
 
